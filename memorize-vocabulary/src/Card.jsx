4923e15df18273dbb01f2de47624946471cec3d8
im-kabir <158508809+im-kabirsumn@users.noreply.github.com>
2024-02-19 12:36:40 +0600

    Create Card.jsx

---
+++ memorize-vocabulary/src/Card.jsx
@@ -0,0 +1,45 @@
+
+
+function Card () {
+  const VOCABULARY_SAMPLE = [
+    {
+      id: 1,
+      word: "Misnomer",
+      phonectics: "/mɪsˈnəʊmə/",
+      defination: "misnaming someone or something"
+    },
+    {
+      id: 2,
+      word: "Source",
+      phonectics: "/sɔːs/",
+      defination: "A point of origin"
+    },
+
+    {
+      id: 3,
+      word: "Procurement",
+      phonectics: "/prəˈkjʊr.mənt, prəˈkjʊə.mənt/",
+      defination: "Process of procuring"
+    },
+    {
+      id: 4,
+      word: "Procure",
+      phonectics: "/prəˈkjʊə,prəˈkjɔː/",
+      defination: "To get something"
+    }
+  ]
+
+  return (
+    <div>
+      <h3>{VOCABULARY_SAMPLE[0].word}</h3>
+      <p>How to pronounce: <span>{VOCABULARY_SAMPLE[0].phonetics}</span></p>
+      <p>Defination: {VOCABULARY_SAMPLE[0].defination</p>
+    
+    </div>
+  )
+}
+
+
+
+
+
